71fe397738286b5fe844629da984d8eafd0d7923
Alexander <adepaolo369@gmail.com>
2024-10-15 17:24:05 -0400

    More splash screen stuff

---
+++ lib/common/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class LuiTextTheme
+{
+  static const TextStyle luiHeader = TextStyle(
+    fontFamily: 'Poppins',
+    color: Colors.white,
+    fontSize: 40,
+    fontStyle: FontStyle.italic,
+    height: 0.7,
+    fontWeight: FontWeight.w600);
+}
--- lib/luisplashscreen.dart
+++ lib/luisplashscreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_font_icons/flutter_font_icons.dart';
-
+import '/common/styles.dart';
 class luiSplashScreen extends StatefulWidget {
   const luiSplashScreen({super.key});
 
@@ -10,9 +10,17 @@ class luiSplashScreen extends StatefulWidget {
 
 class _luiSplashScreenState extends State<luiSplashScreen> {
   @override
+
   Widget build(BuildContext context) {
+    double currentheight;
+    double currentwidth;
+
+
     return Scaffold(
    body: Container(
+     width: double.infinity,
+     height: double.infinity,
+     alignment: Alignment.center,
      decoration: BoxDecoration(
        gradient: LinearGradient(
          colors:[Colors.lightGreenAccent,Colors.green],
@@ -20,6 +28,22 @@ class _luiSplashScreenState extends State<luiSplashScreen> {
          end: Alignment.bottomLeft,
        ),
      ),
+     child: Wrap(
+         crossAxisAlignment: WrapCrossAlignment.center,
+         direction: Axis.vertical,
+        verticalDirection: VerticalDirection.down,
+       alignment: WrapAlignment.center,
+       children: const [
+         Icon(
+         WeatherIcons.wi_raindrop,
+         size: 250,
+         color: Colors.lightBlue,
+         ),
+         Text('Lush Universal Irrigator',
+           style: LuiTextTheme.luiHeader,
+         )
+         ]
+     )
    ),
     );
   }
