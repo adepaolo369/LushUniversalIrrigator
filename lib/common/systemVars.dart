238239c244554450e37117cacf4e1a253c6fd024
Alexander <adepaolo369@gmail.com>
2024-10-21 23:48:53 -0400

    Add Initial Valve object with JSON encoding and decoding to store within a shared preferences list.

--- lib/common/systemVars.dart
+++ lib/common/systemVars.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'dart:convert';
 class SystemInfoHandler
 {
    static const setupComplete = 'setupComp';
    static const deviceID = Null;
+
 
    static final SystemInfoHandler instance = SystemInfoHandler._internal();
 
@@ -15,4 +16,33 @@ class SystemInfoHandler
    }
 
    SystemInfoHandler._internal();
+
+
+
+}
+
+
+
+class Valve
+{
+   int time ;
+   int waterAmount;
+
+   //
+   Valve({required this.time, required this.waterAmount});
+
+   // Convert a valve object to a Map (for JSON encoding).
+   Map<String, dynamic> toJson() => {
+      'time': time,
+      'waterAmount': waterAmount,
+   };
+
+   // Convert a Map to a Valve object (for JSON decoding).
+   factory Valve.fromJson(Map<String, dynamic> json)
+   {
+      return Valve(
+         time: json['time'],
+         waterAmount: json['waterAmount'],
+      );
+   }
 }
